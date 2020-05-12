257e933f1ecd30f151d7f9c2d5b4be4c31a33b6d
Zaralan <Zaralan@users.noreply.github.com>
2020-05-12 16:27:57 -0700

    123

--- Midterm/js/main.js
+++ Midterm/js/main.js
@@ -5,3 +5,29 @@ function googleTranslateElementInit() {
         { pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
         'google_translate_element');
 }
+
+function mobMenu() {
+    var openButton = document.getElementsByClassName('mobOpen')[0],
+        closeButton = document.getElementsByClassName('mobClose')[0],
+        menu = document.getElementById('mobileMenu');
+
+    openButton.addEventListener('click', function () {
+        this.style.display = 'none';
+        closeButton.style.display = 'block';
+        menu.style.display = 'flex';
+    });
+
+    closeButton.addEventListener('click', function () {
+        this.style.display = 'none';
+        openButton.style.display = 'block';
+        menu.style.display = 'none';
+    });
+
+    menu.addEventListener('click', function () {
+        this.style.display = 'none';
+        openButton.style.display = 'block';
+        closeButton.style.display = 'none';
+    });
+}
+
+mobMenu();
